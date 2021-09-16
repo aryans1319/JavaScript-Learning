98757f33a4926aab16d4fd53921fe90b8122c329
Aryan Shaw <aryan4p130202@gmail.com>
2021-09-16 15:02:42 +0530

    Create basicform.js

---
+++ 01Basics/basicform.js
@@ -0,0 +1,27 @@
+const uid="abc12345";
+
+var fullName="Aryan Shaw";
+var email="aryan@dev.co"
+var state="West Bengal";
+var country="India";
+var password="abs12";
+var confirmPassword="abs12";
+var courseCount=0;
+var isLoggedInFromGoogle= false;
+
+//fullName=prompt("Enter your name");
+// taking input
+
+//one way of output
+
+// console.log(uid);
+// console.log("Full name is: ",fullName);
+// console.log(email);
+console.log(`
+
+With Unique id : ${uid}
+user is : ${fullName}
+state : ${state}
+and his email is :${email}
+his password: ${password}
+`)
